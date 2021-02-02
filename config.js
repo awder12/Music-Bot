10001473eff7dcc3b011fdf69b01434ca507630e
awder12 <78434441+awder12@users.noreply.github.com>
2021-02-02 21:27:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.Token = ""; //Bot Token - Important
-exports.Owner = ""; //Bot Owner ID - Important
-exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
-exports.Support = ``; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+exports.Token = "ODA2MjI4MzU0ODk3NDEyMTA2.YBmYqg.o6Cbuk1LJzeJvx5qND42hvRsOPc"; //Bot Token - Important
+exports.Owner = "724188075894374402"; //Bot Owner ID - Important
+exports.Default_Prefix = "¥"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "black"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Support = `https://discord.gg/ErnEW4Ebrp`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
